f921ace224b3f36041279fbf42b689903938bb3e
cmj0415 <cmj51406002@gmail.com>
2025-08-05 22:35:29 +0800

    feat: set up inputs for query

--- dsa-visualization/src/components/SegQuery.tsx
+++ dsa-visualization/src/components/SegQuery.tsx
@@ -1,3 +1,33 @@
+import { useState, useRef } from "react";
+import Button from "./Button";
+
 export default function SegQuery() {
-  return <h1>Query</h1>;
+  const [fromInput, setFromInput] = useState(0);
+  const [toInput, setToInput] = useState(0);
+  const [from, setFrom] = useState(0);
+  const [to, setTo] = useState(0);
+
+  const handleFromChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFromInput(parseInt(event.target.value));
+  };
+  const handleToChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setToInput(parseInt(event.target.value));
+  };
+  const handleConfirm = () => {
+    setFrom(fromInput);
+    setTo(toInput);
+  };
+
+  return (
+    <div>
+      <p>Enter the range you want to query:</p>
+      <div>
+        From:
+        <input name="from" value={fromInput} onChange={handleFromChange} />
+        To:
+        <input name="to" value={toInput} onChange={handleToChange} />
+        <Button text="Confirm" onClickCallback={handleConfirm} />
+      </div>
+    </div>
+  );
 }
