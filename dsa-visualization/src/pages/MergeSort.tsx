61c0d07cc9ff34d43cc68d0beb229081ade8c222
Penguin5728 <georgewu0408@gmail.com>
2025-08-07 21:14:05 +0800

    feat/add functions of mergesort and merge

--- dsa-visualization/src/pages/MergeSort.tsx
+++ dsa-visualization/src/pages/MergeSort.tsx
@@ -14,10 +14,36 @@ type index = {
   value: number;
 };
 
-function merge{
-  l: number,
-  r: Number, 
+const array = new Array(16);
 
-}: void {
+function merge(arr: number[], l: number, mid: number, r: number): void {
+  const n = l + r;
+  let lcount = 0;
+  let rcount = mid;
+  let newarray: number[] = [];
+  for (let i = 0; i < n; i++) {
+    if (arr[lcount] <= arr[rcount]) {
+      newarray[i] = arr[lcount];
+      lcount++;
+    } else if (arr[lcount] > arr[rcount]) {
+      newarray[i] = arr[rcount];
+      rcount++;
+    } else if (lcount === mid) {
+      newarray[i] = arr[rcount];
+      rcount++;
+    } else {
+      newarray[i] = arr[lcount];
+      lcount++;
+    }
+  }
+  for (let i = 0; i < n; i++) arr[i] = newarray[i];
+}
 
+function mergesort(arr: number[], l: number, r: number): void {
+  if (l < r) {
+    const mid = Math.floor((l + r) / 2);
+    mergesort(arr, l, mid);
+    mergesort(arr, mid + 1, r);
+    merge;
+  }
 }
