b98c03ca244230f80eee637a699a2d230ecd3d69
Penguin5728 <georgewu0408@gmail.com>
2025-08-08 11:49:38 +0800

    bugfix/fix some fatal error of merge sort

--- dsa-visualization/src/pages/MergeSort.tsx
+++ dsa-visualization/src/pages/MergeSort.tsx
@@ -10,33 +10,29 @@ type Props = {
   onUpdate: (val: number) => void;
 };
 
-type index = {
-  value: number;
-};
-
 const array = new Array(16);
 
 function merge(arr: number[], l: number, mid: number, r: number): void {
-  const n = l + r;
-  let lcount = 0;
-  let rcount = mid;
+  const n = r - l + 1;
+  let lcount = l;
+  let rcount = mid + 1;
   let newarray: number[] = [];
   for (let i = 0; i < n; i++) {
-    if (arr[lcount] <= arr[rcount]) {
+    if (lcount === mid) {
+      newarray[i] = arr[rcount];
+      rcount++;
+    } else if (rcount === r) {
       newarray[i] = arr[lcount];
       lcount++;
-    } else if (arr[lcount] > arr[rcount]) {
-      newarray[i] = arr[rcount];
-      rcount++;
-    } else if (lcount === mid) {
-      newarray[i] = arr[rcount];
-      rcount++;
+    } else if (arr[lcount] <= arr[rcount]) {
+      newarray[i] = arr[lcount];
+      lcount++;
     } else {
-      newarray[i] = arr[lcount];
-      lcount++;
+      newarray[i] = arr[rcount];
+      rcount++;
     }
   }
-  for (let i = 0; i < n; i++) arr[i] = newarray[i];
+  for (let i = l, j = 0; i < r; i++, j++) arr[i] = newarray[j];
 }
 
 function mergesort(arr: number[], l: number, r: number): void {
@@ -44,6 +40,6 @@ function mergesort(arr: number[], l: number, r: number): void {
     const mid = Math.floor((l + r) / 2);
     mergesort(arr, l, mid);
     mergesort(arr, mid + 1, r);
-    merge;
+    merge(arr, l, mid, r);
   }
 }
